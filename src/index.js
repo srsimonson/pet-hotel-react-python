94a8988c241f8c72ff309a9fe03fec89d4d2f831
Steve Simonson <srsimonson@gmail.com>
2020-07-16 11:27:02 -0500

    updated index

--- src/index.js
+++ src/index.js
@@ -4,27 +4,57 @@ import './index.css';
 import App from './components/App/App.js';
 import { Provider } from 'react-redux';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { takeEvery, put } from 'redux-saga/effects';
+import logger from 'redux-logger';
 import createSagaMiddleware from 'redux-saga';
+import axios from 'axios';
 
-// import Axios from 'axios';
-
-function* rootSaga() {
- 
+function* getPets(){
+  const petInfo = yield axios.get(`/pets`);
+  yield put({type: 'SET_PETS', payload: petInfo.data});
 }
 
-// Create sagaMiddleware
+function* getOwners(){
+  const ownerInfo = yield axios.get(`/owners`);
+  yield put({type: 'SET_OWNERS', payload: ownerInfo});
+}
+
+const petsPeducer = (state = [], action) => {
+  switch (action.type) {
+    case 'SET_PETS':
+      return action.payload;
+      default:
+        return state;
+  }
+}
+
+const ownerReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'SET_OWNERS':
+      return action.payload;
+      default:
+        return state;
+  }
+}
+
 const sagaMiddleware = createSagaMiddleware();
 
-const storeInstance = createStore(
+const storeInstance = createStore (
   combineReducers({
-      
+    petsPeducer,
+    ownerReducer
   }),
-  // Add sagaMiddleware to our store
-  applyMiddleware(sagaMiddleware), //logger
+  applyMiddleware(sagaMiddleware, logger)
 );
 
-// Pass rootSaga into our sagaMiddleware
+
 sagaMiddleware.run(rootSaga);
+
+
+function* rootSaga(){
+  yield takeEvery('GET_PETS', getPets);
+  yield takeEvery('GET_OWNERS', getOwners);
+}
 
 
 ReactDOM.render(<Provider 
